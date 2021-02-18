00ee13c07c052d0a82cc9d9595b17b9c04bd4b24
Vinil Rathod <viniljain703@gmail.com>
2021-02-18 13:00:58 +0530

    7th Commit

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use('/user',userRoute);
