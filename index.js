a16bc1bd175bfd45f6d2f5e70455b3ff78950223
Vinil Rathod <viniljain703@gmail.com>
2021-02-18 15:15:10 +0530

    7th Commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin','*');
@@ -19,8 +20,7 @@ app.use((req,res,next) => {
 
     next();
 });
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:false}));
+
 app.use('/user',userRoute);
 app.use('/post',postRoute);
 app.listen(process.env.PORT || 3001);
