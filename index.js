3c796cf946be5126ab91e7202b75c4f0d343bf12
Vinil Rathod <viniljain703@gmail.com>
2021-02-18 12:23:18 +0530

    6th Commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://vi-post.netlify.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
