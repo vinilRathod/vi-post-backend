e11bf8547547c2804a4dec6509a734bbbd1a6c91
Vinil Rathod <viniljain703@gmail.com>
2021-02-18 14:41:38 +0530

    7th Commit

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use('/user',userRoute);
